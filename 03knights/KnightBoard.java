0e165322a6ab0e77ba832b54c79185bd07d63ba5
nicolecheng <ncheng@stuy.edu>
2016-02-12 11:57:22 -0500

    knightboard set up

---
+++ 03knights/KnightBoard.java
@@ -0,0 +1,19 @@
+public class KnightBoard{
+
+    private int[][]board;
+
+    public KnightBoard(int size){
+	board = new int[size][size];
+    }
+
+    public boolean solve(){
+
+
+
+    }
+
+    void printSolution(){
+
+    }
+
+}
