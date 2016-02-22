aa57de45f198296498027798cf17953056d17de3
nicolecheng <ncheng@stuy.edu>
2016-02-22 16:29:01 -0500

    import file working

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -1,13 +1,45 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Bronze{
+
+    private boolean DEBUG = true;
 
     int[][]grid;
 
-    public Bronze(){}
+    public Bronze(){
+	importFile();
+    }
 
+    public boolean importFile(){
+	File f = new File("makelake.in");
+	try{
+	    Scanner s = new Scanner(f);
+	    debug("success");
+	    
+	    return true;
+	}catch(Exception e){
+	    System.out.println("invalid file");
+	    return false;
+	}
+    }
+
+    public void debug(String s){
+	if(DEBUG){
+	    System.out.println(s);
+	}
+    }
+    
     /*
 file input
 print output
     */
 
+    public static void main(String[]args){
+
+	Bronze b = new Bronze();
+	b.importFile();
+
+    }
 
 }
