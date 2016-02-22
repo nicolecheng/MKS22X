d6b80919128cf9954a461e51b2c816acf9e218dd
nicolecheng <ncheng@stuy.edu>
2016-02-22 16:55:05 -0500

    importing more

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -6,9 +6,14 @@ public class Bronze{
     private boolean DEBUG = true;
 
     int[][]grid;
+    int rows;
+    int cols;
+    int elevation;
+    int num;
 
     public Bronze(){
 	importFile();
+	grid = new int[rows][cols];
     }
 
     public boolean importFile(){
@@ -16,11 +21,45 @@ public class Bronze{
 	try{
 	    Scanner s = new Scanner(f);
 	    debug("success");
-	    
+	    int arg = 0;
+	    while(s.hasNext()){
+		String ct = s.next();
+		int current = Integer.parseInt(ct);
+		//debug(ct);
+		if(arg==0){
+		    rows = current-1;
+		}else if (arg==1){
+		    cols = current-1;
+		}else if(arg==2){
+		    elevation = current;
+		}else if(arg==3){
+		    num = current;
+		}else{
+		    //grid = new int[rows][cols];
+		    for(int r = 0; r < rows; r++){
+			for(int c = 0; c < cols; c++){
+			    grid[r][c] = current;
+			}			
+		    }
+		}
+		arg++;
+		//grid = int[
+	    }
+	    //while(s.hasNext()){
+		
 	    return true;
 	}catch(Exception e){
 	    System.out.println("invalid file");
 	    return false;
+	}
+    }
+
+    public void printGrid(){
+	for(int r = 0; r < rows; r++){
+	    for(int c = 0; c < cols; c++){
+	        System.out.print(grid[r][c]);
+	    }
+	    System.out.println();
 	}
     }
 
@@ -31,14 +70,15 @@ public class Bronze{
     }
     
     /*
-file input
-print output
+      file input
+      print output
     */
 
     public static void main(String[]args){
 
 	Bronze b = new Bronze();
-	b.importFile();
+	//b.importFile();
+	b.printGrid();
 
     }
 
