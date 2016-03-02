9e4ac2613fb0906ddf3e744eb19f17790ce92ae0
nicolecheng <ncheng@stuy.edu>
2016-03-01 20:36:55 -0500

    ok one bug fixed

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -38,61 +38,30 @@ public class Silver{
     }
 
     public boolean loadGrid(){
-	Scanner in = new Scanner(scan);
-	String line = "";
-	int lineNum = 0;
-	int arg = 0;
-	while(in.hasNextLine()){
-	    line = in.nextLine();
-	    if(lineNum==0){
-		Scanner a = new Scanner(line);
-		while(a.hasNext()){
-		    if(arg==0){
-			rows = Integer.parseInt(a.next());
-		    }else if(arg==1){
-			cols = Integer.parseInt(a.next());
-			grid = new char[rows][cols];
-		    }else if(arg==2){
-			steps = Integer.parseInt(a.next());
-		    }
-		    lineNum++;
-		}
-		debug(""+rows+" "+cols+" "+steps);
-	    }else if(lineNum<rows+1){
-		for(int c = 0; c < cols; c++){
-		    if(c==cols-1){
-			if(line.substring(c).equals(".")){
-			    grid[lineNum-1][c] = '.';
-			}else if(line.substring(c).equals("*")){
-			    grid[lineNum-1][c] = '.';
-			}
-		    }else{
-			if(line.substring(c,c+1).equals(".")){
-			    grid[lineNum-1][c] = '.';
-			}else if(line.substring(c,c+1).equals("*")){
-			    grid[lineNum-1][c] = '.';
-			}
-			//grid[lineNum-1][c] = line.substring(c,c+1);
-		    }
+	String cur = "";
+	int row = 0;
+	Scanner n = new Scanner(scan);
+	rows = Integer.parseInt(n.next());
+	cols = Integer.parseInt(n.next());
+	steps = Integer.parseInt(n.next());
+	grid = new char[rows][cols];
+	while(n.hasNext()){
+	    cur = n.next();
+	    if(row<rows){
+	        for(int c = 0; c < cols; c++){
+		    grid[row][c] = cur.charAt(c);
 		}
 	    }else{
-		Scanner m = new Scanner(line);
-		arg = 0;
-		if(arg==0){
-		    startX = Integer.parseInt(m.next());
-		}else if(arg==1){
-		    startY = Integer.parseInt(m.next());
-		}else if(arg==2){
-		    endX = Integer.parseInt(m.next());
-		}else if(arg==3){
-		    endY = Integer.parseInt(m.next());
-		}else{
-		    System.out.println("um this is supposed to be an unreachable statement");
-		}
+		startX = Integer.parseInt(cur);
+		startY = Integer.parseInt(n.next());
+		endX = Integer.parseInt(n.next());
+		endY = Integer.parseInt(n.next());
 	    }
-	}
-	//	debug(""+rows+" "+cols+" "+steps+" "+startX+" "+startY+" "+endX+" "+endY);
-	return false;
+	    
+	    row++;
+	}	
+	debug(""+rows+" "+cols+" "+steps+" \n"+startX+" "+startY+" "+endX+" "+endY);
+	return true;
     }
 
     public void printGrid(){
@@ -116,7 +85,6 @@ public class Silver{
 	
 	Silver fox = new Silver();
 	fox.printGrid();
-	System.out.println("printttt");
 
 	
     }
