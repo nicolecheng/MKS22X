f30a9fe68ec9b93801eb64d15a15724b96fde093
nicolecheng <ncheng@stuy.edu>
2016-03-01 21:34:10 -0500

    another update

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -1,5 +1,6 @@
 // Test case 1, result = 114384
 // Test case 2, result = 81891116
+// Test case 3, result = 1
 
 import java.util.Scanner;
 import java.io.*;
@@ -12,15 +13,18 @@ public class Silver{
     int rows;
     int cols;
     int steps;
-    char[][]grid;
-    char[][]hold;
+    char[][]grid1;
+    int[][]grid;
+    int[][]hold;
     int startX, startY, endX, endY;
 
     public Silver(){
 	importFile();
 	loadGrid();
+	grid = new int[rows][cols];
+	setBoard();
 	hold = grid;
-	grid[startX][startY]=0;
+	grid[startX][startY]=1;
     }
 
     public boolean importFile(){
@@ -45,18 +49,18 @@ public class Silver{
 	rows = Integer.parseInt(n.next());
 	cols = Integer.parseInt(n.next());
 	steps = Integer.parseInt(n.next());
-	grid = new char[rows][cols];
+	grid1 = new char[rows][cols];
 	while(n.hasNext()){
 	    cur = n.next();
 	    if(row<rows){
 	        for(int c = 0; c < cols; c++){
-		    grid[row][c] = cur.charAt(c);
+		    grid1[row][c] = cur.charAt(c);
 		}
 	    }else{
-		startX = Integer.parseInt(cur);
-		startY = Integer.parseInt(n.next());
-		endX = Integer.parseInt(n.next());
-		endY = Integer.parseInt(n.next());
+		startX = Integer.parseInt(cur)-1;
+		startY = Integer.parseInt(n.next())-1;
+		endX = Integer.parseInt(n.next())-1;
+		endY = Integer.parseInt(n.next())-1;
 	    }
 	    
 	    row++;
@@ -65,43 +69,75 @@ public class Silver{
 	return true;
     }
 
-    public void solve(){
-
-
-    }
-
-    public void move(){
-	for(int r = 0; r < rows; r++){
-	    for(int c = 0; c < cols; c++){
-		if((grid[r][c])!='.' && (grid[r][c])!='*'){
-		    add(r,c);
+    public void setBoard(){
+	for (int r = 0; r < rows; r++){
+	    for (int c = 0; c < cols; c++){
+		if(grid1[r][c]=='.'){
+		    grid[r][c]=0;
+		}else if(grid1[r][c]=='*'){
+		    grid[r][c]=-1;
 		}
 	    }
 	}
-
+    }
+    
+    public int move(){
+	//for(int i = 0; i < steps; i++){
+	    for(int r = 0; r < rows; r++){
+		for(int c = 0; c < cols; c++){
+		    if(grid[r][c]>0){// && (grid[r][c])!='.' && (grid[r][c])!='*'){
+			debug(""+r+"  "+c);
+			debug(""+grid[r][c]);
+			add(r,c);
+			//printGrid();
+			//printHold();
+		    }
+		}
+	    }
+	    grid = hold;
+	    //}
+	return grid[endX][endY];
     }
 
-    public boolean add(int r, int c){
+    public void add(int r, int c){
 	if(inBounds(r,c-1)){
-	    hold[r][c-1] = 
+	    hold[r][c-1] = (grid[r][c-1])+1;
+	    //debug("here1");
 	}
 	if(inBounds(r,c+1)){
+	    hold[r][c+1] = (grid[r][c+1])+1;
+	    //debug("here2");
 	}
 	if(inBounds(r-1,c)){
+	    hold[r-1][c] = (grid[r-1][c])+1;
+	    //debug("here3");
 	}
 	if(inBounds(r+1,c)){
+	    hold[r+1][c] = (grid[r+1][c])+1;
+	    //debug("here4");
 	}
-	return true;
+	//debug("here");
+        //grid = hold;
     }
 
     public boolean inBounds(int r, int c){
-	return (r > 0 && r < rows && c > 0 && c < cols && grid[r][c] != '*');
+	return (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] != -1);
     }
 
     public void printGrid(){
 	for(int r = 0; r < rows; r++){
 	    for(int c = 0; c < cols; c++){
 	        System.out.print(grid[r][c]);
+		System.out.print(" ");
+	    }
+	    System.out.println();
+	}
+    }
+
+     public void printHold(){
+	for(int r = 0; r < rows; r++){
+	    for(int c = 0; c < cols; c++){
+	        System.out.print(hold[r][c]);
 		System.out.print(" ");
 	    }
 	    System.out.println();
@@ -118,7 +154,9 @@ public class Silver{
     public static void main(String[]args){
 	
 	Silver fox = new Silver();
+	fox.move();
 	fox.printGrid();
+	
 
 	
     }
