e38a76b7ab63e68be05260c57339d883993daf12
nicolecheng <ncheng@stuy.edu>
2016-03-06 19:14:41 -0500

    still being buggy :(

--- 06Merge/MergeSort.java
+++ 06Merge/MergeSort.java
@@ -1,39 +1,24 @@
 public class MergeSort{
     
-    int[]data;
-    
-    public MergeSort(int[]data){
-	this.data = data;
-    }
-
-    public int[] solve(){//int[]data){
-        int[]hold=new int[data.length];
-	int count=1;
-	int start=0;
+    public static int[] mergesort(int[]data){
+	if(data.length<2){return data;}
+	int[]a=new int[data.length/2];
+	int[]b=new int[data.length-a.length];
 	for(int i = 0; i < data.length; i++){
-	    int[]a=new int[count];
-	    int[]b=new int[count];
-	    int[]c=new int[count*2];
-	    for(int m = 0; m < count; m++){
-		a[m]=data[m+start];
-		b[m]=data[m+start+count];
+	    if(i<a.length){
+		a[i]=data[i];
+	    }else{
+		b[i-a.length]=data[i];
 	    }
-	    c=merge(a,b);
-	    for(int n = 0; n < a.length+b.length; n++){
-		hold[n+start]=c[n];
-	    }
-	    //data=hold;
-	    start+=count*2;
-	    if(start==data.length){
-		start=0;
-		count*=2;
-	    }
-	    //count++;
 	}
+	a=mergesort(a);
+	b=mergesort(b);
+	data=merge(a,b);
 	return data;
     }
 
-    public int[] merge(int[]aryA, int[]aryB){
+
+    public static int[] merge(int[]aryA, int[]aryB){
 	int len = aryA.length+aryB.length;
 	int[]list = new int[len];
 	int a = 0;
@@ -81,49 +66,33 @@ public class MergeSort{
     }
 
 
-    public boolean lessThan(int a, int b){
+    public static boolean lessThan(int a, int b){
 	return (a < b);
     }
 
-
-    /*
-      Preconditions:
-      data.length >= 2
-      the elements of data are in ascending order (sorted)
-      startA <= endA < startB <= endB
-      startA >= 0
-      endB < data.length
-    */
-    
-    //    void merge(int[]data, int startA, endA, startB, endB)
-
-    public void printList(int[]j){
+    public static void printList(int[]j){
 	System.out.print("{");
-	for(int i = 0; i < j.length-1; i++){
-	    System.out.print(j[i]+",");
+	if(j.length>0){
+	    for(int i = 0; i < j.length-1; i++){
+		System.out.print(j[i]+",");
+	    }
+	    System.out.print(j[j.length-1]);
 	}
-	System.out.print(j[j.length-1]+"}");
+	System.out.print("}");
     }
 
-    public void printList(){
-	System.out.print("{");
-	for(int i = 0; i < data.length-1; i++){
-	    System.out.print(data[i]+",");
-	}
-	System.out.print(data[data.length-1]+"}");
-    }
-    
     public static void main(String[]args){
 
 	int[]a = {1,7,10,14};
 	int[]b = {-2,3,4,21,32,47};
-	MergeSort m = new MergeSort(a);
-	m.printList(a);
-	System.out.println();
+	//MergeSort m = new MergeSort(a);
+	//m.printList(a);
+	//System.out.println();
 	//m.printList(b);
-	System.out.println();
-	m.printList(m.solve());//merge(a,b));
-	
+	//System.out.println();
+	MergeSort.printList(merge(a,b));
+	int[]ret = MergeSort.mergesort(b);
+	MergeSort.printList(ret);
     }
     
 }
