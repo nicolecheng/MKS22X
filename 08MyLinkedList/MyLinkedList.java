8961e3bdd33234ef05de0525b831df7ec342fea0
nicolecheng <ncheng@stuy.edu>
2016-03-16 03:13:22 -0400

    get(index), size(), indexOf(val) working

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -8,25 +8,42 @@ public class MyLinkedList{
     public MyLinkedList(){
 	size = 0;
     }
-    
-    public boolean add(int value){
-	if(size==0){
-	    start = new LNode(value);
-	}else{
-	    LNode next = new LNode(value);
-	    LNode current = start;
-	    while(current.getNext()!=null){
-		current = current.getNext();
-	    }
-		current.setNext(next);
+
+    public int get(int index){
+	// throw exception if index >= size
+	LNode current = start;
+        for(int i = 0; i < index; i++){
+	    current = current.getNext();
 	}
-	size++;
-	return true;
+	return current.getValue();
     }
-    
-    public boolean add(int index, int value){
+
+    public boolean set(int index, int newValue){
 	if(index >= size){
 	    return false;
+	}else{
+	    LNode current = start;
+	    for(int i = 0; i < index; i++){
+		current = current.getNext();
+	    }
+	    current.setValue(newValue);
+	}	    
+	return true;
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public int remove(int index){
+	return -1;
+    }
+
+ public boolean add(int index, int value){
+	if(index > size){
+	    return false;
+	}else if(index==size){
+	    add(value);
 	}else{
 	    LNode current = start;
 	    LNode next;
@@ -49,9 +66,38 @@ public class MyLinkedList{
 		    add(hold);
 		}
 	    }
-	    return true;
 	}
+	return true;
     }
+
+    public boolean add(int value){
+	if(size==0){
+	    start = new LNode(value);
+	}else{
+	    LNode next = new LNode(value);
+	    LNode current = start;
+	    while(current.getNext()!=null){
+		current = current.getNext();
+	    }
+		current.setNext(next);
+	}
+	size++;
+	return true;
+    }
+
+    public int indexOf(int value){
+	int ind = 0;
+	LNode current = start;
+	while(current!=null && current.getNext()!=null){
+	    if(current.getValue()==value){
+		return ind;
+	    }else{
+		current = current.getNext();
+		ind++;
+	    }
+	}
+	return -1;
+    }       
     
     public String toString(){
 	LNode current = start;
@@ -69,20 +115,6 @@ public class MyLinkedList{
 	return s + "]";
     }
     
-
-
-    public boolean set(int index, int newValue){
-	if(index >= size){
-	    return false;
-	}else{
-	    LNode current = start;
-	    for(int i = 0; i < index; i++){
-		current = current.getNext();
-	    }
-	    current.setValue(newValue);
-	}	    
-	return true;
-    }
     
     private class LNode{
 
@@ -133,19 +165,16 @@ public class MyLinkedList{
     public static void main(String[]args){
 	MyLinkedList m = new MyLinkedList();
 	m.add(8); // [8]
-	debug(m.size);
 	m.add(3); // [8,3]
-	debug(m.size);
 	m.add(5); // [8,3,5]
-	debug(m.size);
 	m.set(0,1); // [1,3,5]
-	debug(m.size);
 	m.add(7); // [1,3,5,7]
-	debug(m.size);
 	m.add(3,-1); // [1,3,5,-1,7]
-	debug(m.size);
 	m.add(0,2); // [2,1,3,5,-1,7]
-	debug(m.size);
+	// debug(m.get(3)); // 5
+	m.add(6,10); // [2,1,3,5,-1,7,10]
+	// debug(m.indexOf(3)); // 2
+	// debug(m.indexOf(4)); // -1
 	System.out.println(m);
     }
     
