e0918ec2cd979faff68a3b6e017ff9928902800e
nicolecheng <ncheng@stuy.edu>
2016-04-03 20:56:01 -0400

    addFirst() and addLast()

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -10,8 +10,10 @@ public class MyDeque<T>{
 	end = 0;
 	size = 0;
     }
+    
 
-    // 0b. You need a private method to grow the array and copy over the values. 
+    // 0b. You need a private method to grow the array and copy over the values.
+    
     private void grow(){
 	T[]hold = new T[size*2];
 	for(int i = 0; i < size; i++){
@@ -36,7 +38,37 @@ public class MyDeque<T>{
 	    return (start-end > 1);
 	}
     }
-        
+
+    
+    // 1. void addFirst(T value),  2. void addLast(T value)
+    // -When the array is full, re-size, then add. 
+    // -No exceptions are required since you will re-size.
+
+    public void addFirst(T value){
+	if(isFull()){
+	    grow();
+	}
+	if(start==0){
+	    start = size-1;
+	}else{
+	    start--;
+	}
+	data[start] = value;
+	size++;
+    }
+
+    public void addLast(T value){
+	if(isFull()){
+	    grow();
+	}
+	if(end==size-1){
+	    end = 0;
+	}else{
+	    end++;
+	}
+	data[end] = value;
+	size++;
+    }        
 
     
 }
