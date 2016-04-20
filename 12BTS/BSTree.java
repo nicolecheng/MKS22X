ca3d80ab947790e233600d3ccc06c269b384fa0e
nicolecheng <ncheng@stuy.edu>
2016-04-20 12:31:42 -0400

    bstree

---
+++ 12BTS/BSTree.java
@@ -0,0 +1,17 @@
+public class BSTree<T extends Comparable<T>>{
+
+    Node root;
+
+    public class Node{
+
+	Node left, right;
+	T data;
+
+	// get/set
+	// data
+	// left
+	// right
+
+    }
+
+}
