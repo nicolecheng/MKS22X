a70248892c6101b8aee9f20849d9b4cf4bb166e2
nicolecheng <ncheng@stuy.edu>
2016-04-21 12:24:57 -0400

    bstree

--- 12BTS/BSTree.java
+++ 12BTS/BSTree.java
@@ -1,17 +1,89 @@
 public class BSTree<T extends Comparable<T>>{
 
-    Node root;
-
-    public class Node{
+    private class Node{
 
 	Node left, right;
 	T data;
 
-	// get/set
-	// data
-	// left
-	// right
+	private Node(T data){
+	    this.data = data;
+	}
 
+	private T get(){
+	    return data;
+	}
+
+	private void set(T val){
+	    data = val;
+	}
+
+	private Node getLeft(){
+	    return left;
+	}
+
+	private Node getRight(){
+	    return right;
+	}
+
+	private int height(){ 
+	    int h = 1;
+	    if(left==null && right==null){
+		return h;
+	    }else if(left==null && right!=null){
+		h+=right.height();
+	    }else if(left!=null && right==null){
+		h+=left.height();
+	    }else{
+		h+=max(left.height(),right.height());
+	    }
+	    return h;
+	}
+
+	private void add(T value){
+	    if(value.compareTo(data)<0){
+		if(left==null){
+		    left = new Node(value);
+		}else{
+		    left.add(value);
+		}
+	    }else{
+		if(right==null){
+		    right = new Node(value);
+		}else{
+		    right.add(value);
+		}
+	    }
+	}
+
+	private String toString(){
+	    return "";
+	}
+
+	private boolean contains(T value){
+	    return false;
+	}
+	
+    }
+
+    private Node root;
+
+    //OUTER methods here are wrapper methods for the root
+    public getHeight(){
+	//call the root's methods
+	//check for empty first!
+	return root.height();
+    }
+
+    public void add(T value){
+	//check for empty before you do things with root.
+    }
+    public String toString(){
+	//check for empty before you do things with root.
+	return "";
+    }
+    public boolean contains(T value){
+	//check for empty before you do things with root.
+	return false;
     }
 
 }
