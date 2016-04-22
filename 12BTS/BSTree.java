0f79571e6a2c53bfeccde1cd4e7fbbcf3a2e17cd
nicolecheng <ncheng@stuy.edu>
2016-04-22 12:12:49 -0400

    toString working

--- 12BTS/BSTree.java
+++ 12BTS/BSTree.java
@@ -5,7 +5,7 @@ public class BSTree<T extends Comparable<T>>{
 	Node left, right;
 	T data;
 
-	private Node(T data){
+	public Node(T data){
 	    this.data = data;
 	}
 
@@ -39,7 +39,7 @@ public class BSTree<T extends Comparable<T>>{
 	    return h;
 	}
 
-	private void add(T value){
+	public void add(T value){
 	    if(value.compareTo(data)<0){
 		if(left==null){
 		    left = new Node(value);
@@ -55,9 +55,9 @@ public class BSTree<T extends Comparable<T>>{
 	    }
 	}
 
-	public String toString(){
-	    return "";
-	}
+	//public String toString(){
+	//    return "";
+	//}
 
 	private boolean contains(T value){
 	    if(data.equals(value)){
@@ -68,38 +68,76 @@ public class BSTree<T extends Comparable<T>>{
 		}else if(left==null && right!=null){
 		    return right.contains(value);
 		}//etc
+		return false;
+	    }
+	
+	}
+    }
+
+	private Node root;
+
+	//OUTER methods here are wrapper methods for the root
+	public int getHeight(){
+	    //call the root's methods
+	    //check for empty first!
+	    if(root==null){
+		return 0;
+	    }
+	    return root.height();
+	}
+
+	public void add(T value){
+	    if(root==null){
+		root = new Node(value);
+	    }else{
+		root.add(value);
+	    }
+	}
+
+	public String toString(){
+	    String s = "";
+	    if(root==null){
+		return "";
+	    }else{
+	       return toString(root); 
+	    }
+	}
+
+	public String toString(Node start){
+	    String s = " "+start.get();
+	    if(start.getLeft()==null){
+		s+=" _";
+	    }else{
+	        s+=toString(start.getLeft());
+	    }
+	    if(start.getRight()==null){
+	        s+=" _";
+	    }else{
+		s+=toString(start.getRight());
+	    }
+	    return s;
+	}
+	/*
+	public boolean contains(T value){
+	    if(root==null){}
+	    //check for empty before you do things with root.
 	    return false;
 	}
+	*/
+
+    public static void main(String[] args) {
+	BSTree<String> t = new BSTree<String>();
+		
+	t.add("b");
+	t.add("a");
+	t.add("c");
+	t.add("f");
+	t.add("e");
+	t.add("g");
+	System.out.println(t);
 	
-    }
-
-    private Node root;
-
-    //OUTER methods here are wrapper methods for the root
-    public int getHeight(){
-	//call the root's methods
-	//check for empty first!
-	if(root==null){
-	    return 0;
-	}
-	return root.height();
-    }
-
-    public void add(T value){
-	if(root==null){}
-	//check for empty before you do things with root.
-    }
-
-    public String toString(){
-	if(root==null){}
-	//check for empty before you do things with root.
-	return "";
-    }
-
-    public boolean contains(T value){
-	if(root==null){}
-	//check for empty before you do things with root.
-	return false;
+	//System.out.println("Contains q: " + contains("q"));
+	//System.out.println("Contains g: " + contains("g"));
     }
 
 }
