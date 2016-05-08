2425a0f5e65570edbb80204b451a63bd39dd5585
nicolecheng <ncheng@stuy.edu>
2016-05-08 00:39:55 -0400

    fixed constructors / wrote add, doubleSize, pushUp, pushDown

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -7,25 +7,18 @@ public class MyHeap<T extends Comparable<T>>{
     private boolean isMax;
 
     public MyHeap(){
-        T[]ary = (T[])new Comparable[1];
-        size = 1;
+        size = 0;
+	data = (T[]) new Comparable(size+1);
+	//data[0] = 0;
     }
     
     public MyHeap(T[]x){
-	size = x.length+1;
-	data = (T[]) new Comparable[size]; //data = new T[size];
-	for(int i = 1; i < size; i++){
-	    data[i]=x[i-1];
+	size = x.length;
+	data = (T[]) new Comparable[size+1]; //data = new T[size];
+	for(int i = 0; i < size; i++){
+	    data[i+1]=x[i];
 	}
-    }
-
-    private void pushDown(int k){
-	//check maxHeap or minHeap
-	if(data[k].compareTo(data[k*2])>0){
-	    swap(k,k*2);
-	}else if(data[k].compareTo(data[k*2+1])<0){
-	    swap(k,k*2+1);
-	}
+	//data[0]=size;
     }
 
     private void swap(int a, int b){
@@ -34,10 +27,52 @@ public class MyHeap<T extends Comparable<T>>{
 	data[b]=hold;
     }
 
+    /**pushDown
+      precondition: datas[k]'s children are valid heaps
+      postconditions:-the element at index k has been 
+                     shifted to the correct spot.
+                     -data[k] and is a valid heap
+   **/
+
+    private void pushDown(int k){
+	if(isMax){
+	    while((data[k*2]!=null && data[k].compareTo(data[k*2])<0) ||
+		  (data[k*2+1]!=null && data[k].compareTo(data[k*2+1])<0)){
+		if(data[k*2+1].compareTo(data[k*2])>0){
+		    swap(k,k*2+1);
+		}else{
+		    swap(k,k*2);
+		}
+	    }
+	}else{
+	    while((data[k*2]!=null && data[k].compareTo(data[k*2])>0) ||
+		  (data[k*2+1]!=null && data[k].compareTo(data[k*2+1])>0)){
+		if(data[k*2+1].compareTo(data[k*2])>0){
+		    swap(k,k*2);
+		}else{
+		    swap(k,k*2+1);
+		}
+	    }
+    }
+
+    /**pushUp
+       precondition: data is a heap with at most one item
+       out of place (element at k)
+       postconditions:-the element at index k has been 
+       shifted to the correct spot.
+       -data is a valid heap
+    **/
+    
     private void pushUp(int k){
-	//check maxHeap or minHeap
-	if(data[k].compareTo(data[k/2])>0){
-	    swap(k,k/2);
+	if(isMax){
+	    while(data[k].compareTo(data[k/2])>0){
+		swap(k,k/2);
+		k /= 2;
+	    }
+	}else{
+	    while(data[k].compareTo(data[k/2]<0)){
+		    swap(k,k/2);
+	    }
 	}
     }
 
@@ -45,11 +80,24 @@ public class MyHeap<T extends Comparable<T>>{
 	
     }
 
-    //public T delete(){}
+    public T delete(){}
 
-    public void add(T x){}
+    public void add(T x){
+	size++;
+	if(size>=data.length){
+	    doubleSize();
+	}
+	data[size] = x;
+	pushUp(size);
+    }
 
-    private void doubleSize(){}
+    private void doubleSize(){
+	T[]a = (T[]) new Comparable[size*2+1];
+	for(int i = 0; i <= size; i++){
+	    a[i] = data[i];
+	}
+	data = a;
+    }
 
     public String toString(){
 	String s = "[";
